a08369bda9503f477f4cbdc80b8192a63a672c8e
mohamed.saleh <mohamedsaleh1984@hotmail.com>
2025-01-15 16:09:02 -0500

    Extract numbers from register lower, higher

--- Projects/challenge_01/challenge_01/main.c
+++ Projects/challenge_01/challenge_01/main.c
@@ -9,5 +9,14 @@ extern INT64 GetNumber();
 int main() {
 	INT64 num = GetNumber();
 	printf("number is %lld\n", num);
+
+	INT64 lower = 0;
+	lower = num & 0x00000000FFFFFFFF;
+
+	INT64 higher = num;
+	higher = higher >> 32;
+
+	printf("RAX register lower %lld and the higher value %lld\n", lower, higher);
+
 	return 0;
 }
