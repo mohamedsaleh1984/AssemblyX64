83115cb42923d53e0df8eabcd1498403eeed04ab
mohamed.saleh <mohamedsaleh1984@hotmail.com>
2025-01-15 16:11:32 -0500

    added comments and beep.

--- Projects/challenge_01/challenge_01/main.c
+++ Projects/challenge_01/challenge_01/main.c
@@ -7,16 +7,23 @@
 extern INT64 GetNumber();
 
 int main() {
+	// call proc from assembly file.
 	INT64 num = GetNumber();
 	printf("number is %lld\n", num);
 
+	// low value in register RAX
 	INT64 lower = 0;
 	lower = num & 0x00000000FFFFFFFF;
 
+	// high value in register RAX
 	INT64 higher = num;
 	higher = higher >> 32;
 
+	// 2sec
+	Beep(lower, 2000);
+
 	printf("RAX register lower %lld and the higher value %lld\n", lower, higher);
+
 
 	return 0;
 }
